8ba41092d35280e430ad8911987b414a9e988978
yates <yatesj3@mail.uc.edu>
2023-11-21 17:14:08 -0500

    Remove unnecessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,8 @@ plugins {
 }
 
 group = "org.yates"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    // https://mvnrepository.com/artifact/org.junit/junit-bom
-    testImplementation("org.junit:junit-bom:5.9.3")
-    // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
-    // https://mvnrepository.com/artifact/org.mockito/mockito-core
-    testImplementation("org.mockito:mockito-core:5.7.0")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
